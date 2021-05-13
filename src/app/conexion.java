cc02cd35b381530d30a8126359762c96172f4e13
chuyo <84111177+chuyo@users.noreply.github.com>
2021-05-12 21:07:07 -0500

    Conexion BD
    java con workbench

---
+++ src/app/conexion.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+
+public class conexion extends javax.swing.JFrame {
+
+    public static final String URL ="jdbc:mysql://localhost:3306/Escuela";
+    public static final String USERNAME ="root";
+    public static final String PASSWORD ="estrella";
+    
+    public conexion() {
+        initComponents();
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btnConectar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnConectar.setText("Conectar");
+        btnConectar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConectarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(151, 151, 151)
+                .addComponent(btnConectar)
+                .addContainerGap(172, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(121, 121, 121)
+                .addComponent(btnConectar)
+                .addContainerGap(156, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnConectarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConectarActionPerformed
+        try{
+             Connection conn = null;
+             conn = getConnection();
+             PreparedStatement ps;
+             ResultSet res;
+             ps = conn.prepareStatement("SELECT * FROM alumnos");
+             res = ps.executeQuery();
+             if(res.next()){
+                JOptionPane.showMessageDialog(null, res.getString("nombre")+ " " + res.getString("edad")); 
+             }
+             conn.close();
+        }catch(Exception e){
+             System.out.println(e);
+        }
+    }//GEN-LAST:event_btnConectarActionPerformed
+
+    public static Connection getConnection(){
+    Connection conn = null;
+    try{
+      Class.forName("com.mysql.jdbc.Driver");  
+      conn = (Connection) DriverManager.getConnection(URL, USERNAME, PASSWORD);
+      JOptionPane.showMessageDialog(null, "Conexion exitosa");
+    }catch(Exception e){
+        System.out.println(e);
+    }
+    return conn;
+}
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(conexion.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new conexion().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnConectar;
+    // End of variables declaration//GEN-END:variables
+}
